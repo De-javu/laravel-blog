403710e8dd5d1109a86db700f806532ff58d16e8
Andres <andrespardo5151@gmail.com>
2025-01-18 12:08:28 -0500

    listar los post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,11 @@
-<h1>Estas en la pagina de posts del index<h1>
+<x-app-layout>
+    @if($posts->isEmpty())
+                        <p>No hay posts disponibles.</p>
+                    @else
+                        <ul class="text-red-500">
+                            @foreach($posts as $post)
+                                <li>{{ $post->title}}</li>                                 
+                            @endforeach
+                        </ul>
+                    @endif
+</x-app-layout>
